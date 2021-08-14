fce9d651d2862cf4408b4e8393d601f7e601b238
reminderclock <reminderclock@naver.com>
2021-08-15 01:03:19 +0900

    feat: Menu ui

--- src/components/Main/MainContainer/MenuContainer.jsx
+++ src/components/Main/MainContainer/MenuContainer.jsx
@@ -1,9 +1,29 @@
+import React, { useContext, useState } from "react";
+import { MenuContext } from "./MainContainer";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const MenuContainer = () => {
-  return <MenuContainerWrapper></MenuContainerWrapper>;
+  const [addSchedule, setAddSchedule] = useState(false);
+  const { menu, setMenu } = useContext(MenuContext);
+  console.log(menu, setMenu);
+  return (
+    <MenuContainerWrapper>
+      <Link to="/schedulePage">
+        <ScheduleButton>일정추가</ScheduleButton>
+      </Link>
+      <button>그룹관리</button>
+    </MenuContainerWrapper>
+  );
 };
+const ScheduleButton = styled.button`
+  background-color: ${({ addSchedule }) => {
+    if (addSchedule === true) return "blue";
+  }};
+`;
 
-const MenuContainerWrapper = styled.div``;
+const MenuContainerWrapper = styled.div`
+  padding: 10px;
+`;
 
 export default MenuContainer;
