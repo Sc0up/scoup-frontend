327b3cc4c36aceb3a152c689930e63e51818a7ec
reminderclock <reminderclock@naver.com>
2021-08-15 01:07:30 +0900

    Feat: header

--- src/components/Schedule/Header.jsx
+++ src/components/Schedule/Header.jsx
@@ -1,9 +1,19 @@
 import styled from "styled-components";
 
 const Header = () => {
-  return <HeaderWrapper></HeaderWrapper>;
+  return (
+    <HeaderWrapper>
+      <TitleInput></TitleInput>
+      <CloseButton>닫기</CloseButton>
+    </HeaderWrapper>
+  );
 };
-
-const HeaderWrapper = styled.div``;
+const TitleInput = styled.input``;
+const CloseButton = styled.div``;
+const HeaderWrapper = styled.div`
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+`;
 
 export default Header;
